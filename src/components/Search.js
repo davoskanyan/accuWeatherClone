ff746fbf5dd508371cc140484edfd4d4e2b2275b
Davit Voskanyan <ysudav@gmail.com>
2023-12-25 11:58:47 +0400

    Search input with autocomplete

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,4 +1,4 @@
-import { TextField } from '@radix-ui/themes';
+import { DropdownMenu, TextField } from '@radix-ui/themes';
 import SearchIcon from '../icons/searchIcon.svg';
 import { useEffect, useState } from 'react';
 
@@ -8,6 +8,10 @@ function Search() {
   const [city] = useState('yerevan');
   const [locationKey, setLocationKey] = useState('');
   const [isLoading, setIsLoading] = useState('false');
+  const [searchValue, setSearchValue] = useState('');
+  const [searchOptions, setSearchOptions] = useState([]);
+  const [open, setOpen] = useState(false);
+  const [error, setError] = useState('');
 
   useEffect(() => {
     async function getLocationKey() {
@@ -19,7 +23,7 @@ function Search() {
         const data = await res.json();
         setLocationKey(data[0].Key);
       } catch (e) {
-        alert(e.message);
+        setError('Cannot fetch data');
       } finally {
         setIsLoading(false);
       }
@@ -38,9 +42,9 @@ function Search() {
           `http://dataservice.accuweather.com/forecasts/v1/daily/1day/${locationKey}/?apikey=${API_KEY}`,
         );
         const data = await res.json();
-        console.log('dv:', data);
+        console.log('data:', data);
       } catch (e) {
-        alert(e.message);
+        setError('Cannot fetch data');
       } finally {
         setIsLoading(false);
       }
@@ -48,19 +52,96 @@ function Search() {
 
     getForecastbyLoactionKey();
   }, [locationKey]);
+
+  useEffect(() => {
+    async function fetchAutocomplete() {
+      if (searchValue === null) return;
+      if (searchValue.length < 2) return;
+      try {
+        setIsLoading(true);
+        const res = await fetch(
+          `http://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=${API_KEY}&q=${searchValue}`,
+        );
+        const data = await res.json();
+        setSearchOptions(data);
+        console.log('autocomplete:', data);
+      } catch (e) {
+        setError('Cannot fetch data');
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    fetchAutocomplete();
+  }, [searchValue]);
+
+  function handleInputChange(e) {
+    setOpen(true);
+    setSearchValue(e.target.value);
+  }
+
+  function handleInputFocus(e) {
+    if (e.relatedTarget?.role === 'menu') {
+      e.target.focus();
+    }
+  }
+
+  function handleSelect() {
+    setOpen(false);
+  }
+
   return (
     <div className="flex justify-center w-full flex-col items-center">
-      <TextField.Root className="h-11 border-white w-[532px] ">
+      <TextField.Root className="h-11 border-white w-[532px] z-40">
         <TextField.Slot>
           <img alt="searchIcon" src={SearchIcon} height="16" width="16" />
         </TextField.Slot>
+
         <TextField.Input
-          className="mx-0 my-auto border-4"
-          placeholder="Search"
+          className="mx-0 my-auto border-4 z-10"
+          placeholder="Search your City"
+          value={searchValue}
+          onBlur={(e) => handleInputFocus(e)}
+          onChange={(e) => handleInputChange(e)}
+          onClick={() => setOpen(true)}
         />
       </TextField.Root>
-      {isLoading && <div>Loading...</div>}
-      <div></div>
+      <DropdownMenu.Root className="z-0" open={open}>
+        <DropdownMenu.Trigger>
+          <div className="relative w-[532px] border-amber-500 border h-[44px] z-0 top-[-44px]"></div>
+        </DropdownMenu.Trigger>
+
+        <DropdownMenu.Content
+          sideOffset={-3}
+          onPointerDownOutside={() => setOpen(false)}
+          alignOffset={1}
+          className="w-[530px] min-h-[44px] p-0"
+        >
+          <DropdownMenu.Group autoFocus={false}>
+            {error && <DropdownMenu.Item>{error}</DropdownMenu.Item>}
+            {isLoading ? (
+              <DropdownMenu.Item>Loading...</DropdownMenu.Item>
+            ) : (
+              searchOptions?.map((city) => (
+                <DropdownMenu.Item
+                  onSelect={() => handleSelect()}
+                  autoFocus={false}
+                  key={city.key}
+                  className="dropdown-item data-[highlighted]:bg-stone-100 data-[highlighted]:text-black py-6"
+                >
+                  <div className="flex flex-col ">
+                    <span className=" font-bold">{city.LocalizedName}</span>
+                    <span className="text-stone-500">
+                      {city.AdministrativeArea.LocalizedName},{' '}
+                      {city.AdministrativeArea.ID} {city.Country.ID}
+                    </span>
+                  </div>
+                </DropdownMenu.Item>
+              ))
+            )}
+          </DropdownMenu.Group>
+        </DropdownMenu.Content>
+      </DropdownMenu.Root>
     </div>
   );
 }
